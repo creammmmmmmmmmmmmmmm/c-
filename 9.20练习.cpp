c8cb5650ea2f1d068f37bcb800990fdd7f64f147
creammmmmmmmmmmmmmmm <113745096+creammmmmmmmmmmmmmmm@users.noreply.github.com>
2022-09-20 17:53:56 +0800

    Add files via upload

--- 9.20练习.cpp
+++ 9.20练习.cpp
@@ -25,31 +25,64 @@
 //}
 //输入密码，限制三次
 
-int main()
-{
-	char password[10]={0};
-	int i = 1;
-	while (i <= 3)
-	{
-		scanf("%s", password);
-		if (strcmp(password,"123456")==0)
-		{
-			printf("密码正确");
-			break;
-		}
-		else
-		{
-			printf("密码错误\n");
-			i++;
-		}
-	}
-	if (i == 4)
-	{
-		printf("别玩我！！！");
-	}
-	return 0;
-}
+//int main()
+//{
+//	char password[10]={0};
+//	int i = 1;
+//	while (i <= 3)
+//	{
+//		scanf("%s", password);
+//		if (strcmp(password,"123456")==0)
+//		{
+//			printf("密码正确");
+//			break;
+//		}
+//		else
+//		{
+//			printf("密码错误\n");
+//			i++;
+//		}
+//	}
+//	if (i == 4)
+//	{
+//		printf("别玩我！！！");
+//	}
+//	return 0;
+//}
 
+
+//复习昨日的二分查找
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k;
+//	scanf("%d", &k);
+//	int left = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (k < arr[mid])
+//		{
+//			right = mid - 1;
+//		}
+//		else if (k > arr[mid])
+//		{
+//			left = mid + 1;
+//		}
+//		else if (k = arr[mid])
+//		{
+//			printf("找到了，下标是%d", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到");
+//	}
+//	return 0;
+//}
 
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
